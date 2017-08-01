fceeff56a54e5bcba5421762cb3bd25ca1f6f8fd
Khalid Alrashed <Alrashed.kh@gmail.com>
2017-07-31 22:34:07 -0700

    Add results view controller

---
+++ BoutTime/ResultsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ResultsViewController.swift
+//  BoutTime
+//
+//  Created by Khalid Alrashed on 7/31/17.
+//  Copyright © 2017 Khalid Alrashed. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    @IBOutlet weak var finalScoreLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func playAgainTapped(_ sender: Any) {
+    }
+    
+}
